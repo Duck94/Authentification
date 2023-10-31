17b09a4bc1f462c1186242f33aec731286523bdd
ATSAMO KENFACK Robernis C. <roberniskenfack@codetechn.com>
2023-10-31 22:26:12 +0300

    ajout md5

--- app1.js
+++ app1.js
@@ -18,6 +18,7 @@ app.use(express.static("public"));
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 
+
 // connecter a moongose
 main().catch(err => console.log(err));
 
